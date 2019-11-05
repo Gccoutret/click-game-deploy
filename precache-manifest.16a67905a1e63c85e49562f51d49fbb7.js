ce3c94e42da419e6ccebadbfe628b33ebb53acc2
GCout <Gccoutret@gmail.com>
2019-11-05 09:54:03 -0600

    Updates

---
+++ precache-manifest.16a67905a1e63c85e49562f51d49fbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bf815ddc898272836504906fa1c04f",
+    "url": "/click-game-deploy/index.html"
+  },
+  {
+    "revision": "fda4849e2418e8fb0157",
+    "url": "/click-game-deploy/static/css/main.64faf731.chunk.css"
+  },
+  {
+    "revision": "1d286d30410675b9ec07",
+    "url": "/click-game-deploy/static/js/2.616f98b2.chunk.js"
+  },
+  {
+    "revision": "fda4849e2418e8fb0157",
+    "url": "/click-game-deploy/static/js/main.4c081488.chunk.js"
+  },
+  {
+    "revision": "301de04041bc4a439b5c",
+    "url": "/click-game-deploy/static/js/runtime-main.66c992bf.js"
+  }
+]);
